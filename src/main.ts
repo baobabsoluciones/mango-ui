d1e0acfaa66eb48403e03ea487c2c798d9845c90
Helena Cana <helena.cana@baobabsoluciones.es>
2024-03-19 11:22:21 +0100

    Fixed vuetify

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 // Plugins
-import { registerPlugins } from '@/plugins'
+import { registerPlugins } from './plugins'
 
-//Styles
-import './assets/styles/main.css';
-import './assets/styles/variables.css'
 
 const app = createApp(App)
 
