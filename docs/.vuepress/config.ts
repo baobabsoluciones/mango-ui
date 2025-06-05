dae238ac32c9e095c181768433a5574f053c4e74
Helena Cana <43758109+HelenaCA@users.noreply.github.com>
2025-06-05 10:55:31 +0200

    Create github docs for mango-ui (#84)
    * Create github docs for mango-ui
    
    * remove msnackbar test for the moment

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defaultTheme } from '@vuepress/theme-default';
 import { defineUserConfig } from 'vuepress';
 
 export default defineUserConfig({
-  base: '/',
+  base: '/mango-ui/',
   title: 'Mango UI',
   description: 'Mango UI is a Vue 3 component library based on Vuetify. This library provides a set of reusable UI components that can be used in any Vue 3 project.',
 
