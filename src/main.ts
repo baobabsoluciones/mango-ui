f383702bd71a2401695fe0e798a830a174cc6e6f
david.hidalgo <david.hidalgo@baobabsoluciones.es>
2025-08-18 16:47:21 +0200

    install primevue

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,23 @@ import '@/assets/styles/style.css'
 import '@/assets/styles/variables.css'
 import App from '@/App.vue'
 import VueApexCharts from 'vue3-apexcharts'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
+// PrimeVue styles
+import 'primevue/resources/themes/aura/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
 
 // Plugins
 import { registerPlugins } from '@/plugins'
 
 const app = createApp(App)
-
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 registerPlugins(app)
 
 app.mount('#app')
