baed714389e56fc4d8e6e2e2fcb5ccf32a9285d2
david.hidalgo <david.hidalgo@baobabsoluciones.es>
2025-08-18 16:39:03 +0200

    update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
-import './assets/styles/style.css'
-import './assets/styles/variables.css'
-import App from './App.vue'
+import '@/assets/styles/style.css'
+import '@/assets/styles/variables.css'
+import App from '@/App.vue'
 import VueApexCharts from 'vue3-apexcharts'
 
 // Plugins
-import { registerPlugins } from './plugins'
+import { registerPlugins } from '@/plugins'
 
 const app = createApp(App)
 
